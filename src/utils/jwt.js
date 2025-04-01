6f70eb87592e07bf421cb838a45e4061618afce3
Aldemylla <aldemylla@gmail.com>
2025-04-01 17:56:21 -0300

    refactor: Update token storage format and add logging for debugging

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,14 +4,19 @@ export function getJwtToken() {
 
     if (!isInIframe) resolve();
 
+    console.log('getJwtToken');
+
     const eventHandler = (event) => {
       if (event.data.event === 'updateToken') {
-        localStorage.setItem('authToken', event.data.token);
+        console.log('updateToken');
+        localStorage.setItem('authToken', `Bearer ${event.data.token}`);
         window.removeEventListener('message', eventHandler);
         return resolve();
       }
     };
     window.addEventListener('message', eventHandler);
+    console.log('postMessage');
+
     window.parent.postMessage({ event: 'getToken' }, '*');
   });
 }
