8c59332d561984aa89a87873270913dbddc3f37c
Aldemylla <aldemylla@gmail.com>
2025-04-03 11:37:49 -0300

    refactor: Simplify template structure by removing unnecessary loading state

--- src/components/Brain/Tunings/SettingsAgentsTeam/index.vue
+++ src/components/Brain/Tunings/SettingsAgentsTeam/index.vue
@@ -1,17 +1,9 @@
 <template>
-  <UnnnicSkeletonLoading v-if="!tuningsStore.settings.status" />
-
-  <section
-    v-else
-    class="settings-agents-team"
-  >
+  <section class="settings-agents-team">
     <AgentsPreview />
   </section>
 </template>
 
 <script setup>
 import AgentsPreview from './AgentsPreview.vue';
-import { useTuningsStore } from '@/store/Tunings';
-
-const tuningsStore = useTuningsStore();
 </script>
