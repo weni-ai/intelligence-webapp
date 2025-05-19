ac9b542ec944ef7339a90ba4416f21b032468939
Aldemylla <aldemylla@gmail.com>
2025-05-19 16:09:36 -0300

    feat: Integrate supervisor export feature flag in SupervisorHeader component

--- src/store/FeatureFlags.js
+++ src/store/FeatureFlags.js
@@ -34,6 +34,7 @@ export const useFeatureFlagsStore = defineStore('FeatureFlags', () => {
       isOrgEnabledForFlag('VITE_FF_ORGS_WITH_AGENTS_TEAM') ||
       isProjectEnabledForFlag('VITE_FF_PROJECTS_WITH_AGENTS_TEAM'),
     upgradeToMultiAgents,
+    supervisorExport: growthbook?.isOn('supervisor_export'),
   }));
 
   function editUpgradeToMultiAgents(value) {
