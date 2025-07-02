d92bfd3714f0de0bbf1c09f030c00a85539d99fe
Aldemylla <aldemylla@gmail.com>
2025-07-02 17:13:34 -0300

    feat: New Supervisor table structure and style

---
+++ src/views/AgentBuilder/Supervisor/SupervisorConversations/ConversationsTable/ConversationRow.vue
@@ -0,0 +1,184 @@
+<template>
+  <tr
+    class="conversation-row"
+    :class="{ 'conversation-row--selected': isSelected }"
+  >
+    <section class="conversation-row__main-infos">
+      <td class="main-infos__avatar">
+        <p class="avatar__letter">{{ conversation?.username?.[0] || '-' }}</p>
+      </td>
+
+      <td class="main-infos__user-data">
+        <ConversationInfos
+          :name="conversation.username"
+          :urn="conversation.urn"
+        />
+      </td>
+
+      <td class="main-infos__status">
+        <UnnnicTag
+          class="cell__status"
+          :scheme="statusProps.scheme"
+          :text="statusProps.text"
+        />
+      </td>
+    </section>
+
+    <section class="conversation-row__secondary-infos">
+      <td
+        v-if="csatProps"
+        class="secondary-infos__csat"
+      >
+        <UnnnicTag
+          :scheme="csatProps.scheme"
+          :text="csatProps.text"
+        />
+      </td>
+
+      <td class="secondary-infos__date">
+        <ConversationDate :date="conversation.created_on" />
+      </td>
+    </section>
+  </tr>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+import i18n from '@/utils/plugins/i18n';
+
+import ConversationDate from './ConversationDate.vue';
+import ConversationInfos from './ConversationInfos.vue';
+
+const props = defineProps({
+  conversation: {
+    type: Object,
+    required: true,
+  },
+  isSelected: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const t = (key) => i18n.global.t(key);
+
+const statusProps = computed(() => {
+  const status = props.conversation.status || 'in_progress';
+
+  const baseStatus = {
+    text: t(`agent_builder.supervisor.status.${status}`),
+  };
+
+  const mapStatus = {
+    in_progress: {
+      scheme: 'aux-blue-500',
+    },
+    resolved: {
+      scheme: 'aux-green-500',
+    },
+    unresolved: {
+      scheme: 'aux-red-500',
+    },
+  };
+
+  return {
+    ...baseStatus,
+    ...mapStatus[status],
+  };
+});
+
+const csatProps = computed(() => {
+  const mapCsat = {
+    very_satisfied: {
+      text: 'Very satisfied',
+    },
+    satisfied: {
+      text: 'Satisfied',
+    },
+    neutral: {
+      text: 'Neutral',
+    },
+    unsatisfied: {
+      text: 'Unsatisfied',
+    },
+    very_unsatisfied: {
+      text: 'Very unsatisfied',
+    },
+  };
+
+  return mapCsat[props.conversation.csat];
+});
+</script>
+
+<style lang="scss" scoped>
+.conversation-row {
+  position: relative;
+
+  border-radius: $unnnic-border-radius-md;
+
+  padding: $unnnic-spacing-xs $unnnic-spacing-sm;
+
+  display: flex;
+  gap: $unnnic-spacing-sm;
+  align-items: center;
+  justify-content: space-between;
+
+  cursor: pointer;
+
+  &--selected,
+  &:hover {
+    background-color: $unnnic-color-background-sky;
+  }
+
+  &::after {
+    content: '';
+    background-color: $unnnic-color-neutral-light;
+
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+
+    width: calc(100% - $unnnic-spacing-sm * 2);
+    height: $unnnic-border-width-thinner;
+  }
+
+  td {
+    padding: 0;
+  }
+
+  &__main-infos,
+  &__secondary-infos {
+    display: flex;
+    gap: $unnnic-spacing-sm;
+    align-items: center;
+  }
+
+  &__main-infos {
+    .main-infos__avatar {
+      min-width: 35px;
+      min-height: 35px;
+
+      border-radius: 50%;
+      background-color: $unnnic-color-weni-100;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .avatar__letter {
+        font-family: $unnnic-font-family-secondary;
+        font-size: $unnnic-font-size-body-gt;
+        font-weight: $unnnic-font-weight-bold;
+        color: $unnnic-color-weni-600;
+      }
+    }
+
+    .main-infos__status,
+    .secondary-infos__date {
+      white-space: nowrap;
+    }
+  }
+}
+</style>
--- src/views/AgentBuilder/Supervisor/SupervisorConversations/ConversationsTable/index.vue
+++ src/views/AgentBuilder/Supervisor/SupervisorConversations/ConversationsTable/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="conversations-table">
-    <ConversationsSearch />
+  <ConversationsSearch />
 
-    <UnnnicTableNext
+  <section class="conversations-table">
+    <!-- <UnnnicTableNext
       v-model:pagination="pagination.page"
       :class="{
         'conversations-table__table': true,
@@ -18,21 +18,41 @@
       :paginationInterval="pagination.interval"
       :isLoading="supervisorStore.conversations.status === 'loading'"
       @row-click="handleRowClick"
+    /> -->
+
+    <UnnnicIntelligenceText
+      class="conversations-table__count"
+      tag="p"
+      color="neutral-clean"
+      family="primary"
+      size="body-gt"
+    >
+      {{
+        $t('agent_builder.supervisor.conversations_count', {
+          count: conversations.count,
+        })
+      }}
+    </UnnnicIntelligenceText>
+
+    <ConversationRow
+      v-for="conversation in conversations.results"
+      :key="conversation.id"
+      :conversation="conversation"
+      :isSelected="conversation.id === supervisorStore.selectedConversation?.id"
+      @click="handleRowClick(conversation)"
     />
   </section>
 </template>
 
 <script setup>
 import { computed, nextTick, ref, watch } from 'vue';
-import Unnnic from '@weni/unnnic-system';
 
 import { useSupervisorStore } from '@/store/Supervisor';
 
 import i18n from '@/utils/plugins/i18n';
 
-import ConversationInfos from './ConversationInfos.vue';
-import ConversationDate from './ConversationDate.vue';
 import ConversationsSearch from './ConversationsSearch.vue';
+import ConversationRow from './ConversationRow.vue';
 
 const t = (key) => i18n.global.t(key);
 const supervisorStore = useSupervisorStore();
@@ -50,62 +70,6 @@ function handleRowClick(row) {
     supervisorStore.selectConversation(row.id);
   });
 }
-
-const table = computed(() => {
-  const getTagProps = (human_support) => {
-    if (human_support) {
-      return {
-        scheme: 'aux-blue-500',
-        leftIcon: 'forward',
-        text: t('agent_builder.supervisor.forwarded_human_support.title'),
-      };
-    }
-
-    return {
-      scheme: 'aux-green-500',
-      leftIcon: 'check',
-      text: t('agent_builder.supervisor.attended_by_agent.title'),
-    };
-  };
-  return {
-    headers: [
-      { content: '' },
-      { content: '', size: 'auto' },
-      { content: '', size: 'auto' },
-    ],
-    rows: conversations.value.results?.map(
-      ({ urn, created_on, last_message, human_support, id }) => {
-        const dateComponent = {
-          component: ConversationDate,
-          props: {
-            date: created_on,
-          },
-        };
-
-        const tagComponent = {
-          component: Unnnic.unnnicTag,
-          props: {
-            type: 'default',
-            ...getTagProps(human_support),
-          },
-        };
-
-        const infoComponent = {
-          component: ConversationInfos,
-          props: {
-            urn,
-            lastMessage: last_message,
-          },
-        };
-
-        return {
-          content: [infoComponent, tagComponent, dateComponent],
-          id,
-        };
-      },
-    ),
-  };
-});
 
 const selectedConversationIndex = computed(() =>
   conversations.value.results?.findIndex(
@@ -169,35 +133,8 @@ watch(
 
 <style scoped lang="scss">
 .conversations-table {
-  display: grid;
-  gap: $unnnic-spacing-xs;
-
-  &__table {
-    :deep(.unnnic-table-next__body) {
-      .unnnic-table-next__body-row {
-        $table-radius: $unnnic-border-radius-md;
-
-        &:first-child {
-          border-radius: $table-radius $table-radius 0 0;
-        }
-
-        &:last-child {
-          border-radius: 0 0 $table-radius $table-radius;
-        }
-      }
-
-      .unnnic-table-next__body-cell:nth-child(2) {
-        padding: $unnnic-spacing-ant 0;
-      }
-    }
-
-    &--with-results {
-      :deep(.unnnic-table-next__body-row):hover {
-        background-color: $unnnic-color-background-sky;
-
-        cursor: pointer;
-      }
-    }
+  &__count {
+    margin-bottom: $unnnic-spacing-xs;
   }
 }
 </style>
