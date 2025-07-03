78ea9cec358fa54de34e3c65c67d7deee471a147
Aldemylla <aldemylla@gmail.com>
2025-07-03 18:11:37 -0300

    feat: Added supervisor header conversations detail button

---
+++ src/components/AgentBuilder/Supervisor/SupervisorHeaderDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <p
+    class="supervisor-header-details"
+    @click="openDetailsModal"
+  >
+    {{ $t('agent_builder.supervisor.learn_details_conversations') }}
+  </p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isDetailsModalOpen = ref(false);
+
+function openDetailsModal() {
+  isDetailsModalOpen.value = true;
+}
+</script>
+
+<style scoped lang="scss">
+.supervisor-header-details {
+  display: initial;
+
+  color: $unnnic-color-neutral-cloudy;
+  font-size: $unnnic-font-size-body-gt;
+  font-weight: $unnnic-font-weight-bold;
+  font-family: $unnnic-font-family-secondary;
+  text-decoration: underline;
+
+  cursor: pointer;
+}
+</style>
