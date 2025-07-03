6ee045a65d1042b907e26de2149d7ac071be53cf
Aldemylla <aldemylla@gmail.com>
2025-07-03 18:54:21 -0300

    feat: Implement details modal for conversation insights in SupervisorHeaderDetails component

--- src/components/AgentBuilder/Supervisor/SupervisorHeaderDetails.vue
+++ src/components/AgentBuilder/Supervisor/SupervisorHeaderDetails.vue
@@ -1,10 +1,32 @@
 <template>
   <p
     class="supervisor-header-details"
-    @click="openDetailsModal"
+    @click="handleDetailsModal"
   >
     {{ $t('agent_builder.supervisor.learn_details_conversations') }}
   </p>
+
+  <UnnnicModalDialog
+    :modelValue="isDetailsModalOpen"
+    showCloseIcon
+    showActionsDivider
+    size="md"
+    title="About conversation"
+    class="supervisor-header-details__modal"
+    @update:model-value="handleDetailsModal"
+  >
+    <section class="modal__topic">
+      <p class="topic__title">What is a conversation?</p>
+      <p class="topic__description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
+    </section>
+
+    <section class="modal__topic">
+      <p class="topic__title">When is a conversation considered resolved?</p>
+      <p class="topic__description">{{ description }}</p>
+    </section>
+  </UnnnicModalDialog>
 </template>
 
 <script setup>
@@ -12,9 +34,17 @@ import { ref } from 'vue';
 
 const isDetailsModalOpen = ref(false);
 
-function openDetailsModal() {
-  isDetailsModalOpen.value = true;
+function handleDetailsModal() {
+  isDetailsModalOpen.value = !isDetailsModalOpen.value;
 }
+
+const description = `A conversation is only considered resolved when the user goes beyond the initial greeting and actually engages:
+- Asks questions;
+- Requests data;
+- Confirms information;
+- Receives guidance that allows them to complete all or part of their request.
+Messages limited to greetings, emojis or closed without useful interaction remain unresolved, while interaction that delivers value to the user is marked as resolved.
+`;
 </script>
 
 <style scoped lang="scss">
@@ -28,5 +58,28 @@ function openDetailsModal() {
   text-decoration: underline;
 
   cursor: pointer;
+
+  &__modal {
+    .modal__topic {
+      margin-bottom: $unnnic-spacing-sm;
+
+      .topic__title {
+        margin-bottom: $unnnic-spacing-nano;
+        color: $unnnic-color-neutral-dark;
+        font-size: $unnnic-font-size-body-gt;
+        font-family: $unnnic-font-family-secondary;
+        line-height: $unnnic-line-height-md + $unnnic-font-size-body-gt;
+        font-weight: $unnnic-font-weight-bold;
+      }
+
+      .topic__description {
+        color: $unnnic-color-neutral-cloudy;
+        font-size: $unnnic-font-size-body-md;
+        font-family: $unnnic-font-family-secondary;
+        line-height: $unnnic-line-height-md + $unnnic-font-size-body-md;
+        white-space: pre-line;
+      }
+    }
+  }
 }
 </style>
