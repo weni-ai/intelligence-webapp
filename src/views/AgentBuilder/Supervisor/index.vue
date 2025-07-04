85ab47f2555ecfe85eac76bc40ddcc0e63597346
Aldemylla <aldemylla@gmail.com>
2025-07-04 16:20:15 -0300

    feat: Adjust grid layout for conversation section to improve responsiveness

--- src/views/AgentBuilder/Supervisor/index.vue
+++ src/views/AgentBuilder/Supervisor/index.vue
@@ -77,11 +77,12 @@ onBeforeMount(async () => {
   display: grid;
   grid-template-rows: auto 1fr;
 
+  min-height: 100%;
+
   overflow: hidden auto;
 
   &--with-conversation {
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: auto 1fr;
+    grid-template-columns: 7fr 5fr;
   }
 
   &__header {
