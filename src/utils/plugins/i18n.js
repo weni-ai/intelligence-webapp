37691c803d10161b46d73c59d5be4fefe50d0d4c
GeorgeLimaDev <george.lima@vtex.com.br>
2026-03-27 08:26:27 -0300

    Add RO translations and app support. Fix EN, ES and PT translations.

--- src/utils/plugins/i18n.js
+++ src/utils/plugins/i18n.js
@@ -3,11 +3,13 @@ import * as VueI18n from 'vue-i18n';
 import en from '../../locales/en.json';
 import ptbr from '../../locales/pt_br.json';
 import es from '../../locales/es.json';
+import ro from '../../locales/ro.json';
 
 const languages = {
   'en-US': en,
   'pt-BR': ptbr,
   es,
+  ro,
 };
 
 const messages = Object.assign(languages);
